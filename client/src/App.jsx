174ea491ddb985d60725a819c56cb7393aea0737
dj12589 <daria.jammal@gmail.com>
2019-08-28 18:23:46 -0700

    initial sizing component implementation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,40 @@
 import ItemDetails from './components/ItemDetails/ItemDetails.jsx';
 import Shipping from './components/Shipping/Shipping.jsx';
+import Sizing from './components/Sizing/Sizing.jsx';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {results: []};
+    this.state = {
+      results: [],
+      sizing: [],
+      questions: []
+    };
 
   }
   componentDidMount() {
-    fetch("http://127.0.0.1:3001/api/items/1")
-      .then(res => res.json())
-      .then(
-        (result) => {
-        this.setState({
-          results: result
-        })
+    var id = 12;
+    Promise.all([fetch("http://127.0.0.1:3001/api/items/" + id),
+      fetch("http://127.0.0.1:3001/api/questions/" + id),
+      fetch("http://127.0.0.1:3001/api/sizing/" + id)
+    ])
+    .then(([res1, res2, res3]) => {
+      return Promise.all([res1.json(), res2.json(), res3.json()])
+    })
+    .then(([res1, res2, res3]) => {
+      this.setState({
+        results: res1,
+        questions: res2,
+        sizing: res3
       })
-
+    })
   }
 
   render() {
 
     if(this.state.results.length) {
-      var currentEle = <Shipping details={this.state.results}/>
+      var currentEle = <Sizing details={this.state.sizing}/>
     }
     return(
       <div>
--- client/src/components/Sizing/Sizing.jsx
+++ client/src/components/Sizing/Sizing.jsx
@@ -1,6 +1,36 @@
-function Sizing() {
+function Sizing(props) {
+
+  console.log(props.details)
+
+  var chart = props.details.map(item => {
+    return(
+      <tbody>
+      <tr>
+        <td>{item.size}</td>
+        <td>{item.neck}</td>
+        <td>{item.chest}</td>
+        <td>{item.sleeve}</td>
+      </tr>
+      </tbody>
+    )
+
+  })
+
   return (
-    <h1>Sizing</h1>
+    <div>
+    <h3>Sizing</h3>
+    <table>
+    <tbody>
+    <tr>
+      <th>Size</th>
+      <th>Neck</th>
+      <th>Chest</th>
+      <th>Sleeve</th>
+    </tr>
+    </tbody>
+    {chart}
+    </table>
+    </div>
   );
 }
 
