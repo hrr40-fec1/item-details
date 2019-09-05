465c201f0d5ffc66ec514d80d4538c55a6c613b9
dj12589 <daria.jammal@gmail.com>
2019-09-05 14:45:58 -0700

    incorp server tests

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,17 @@ module.exports = {
   verbose: true,
   "modulePaths": ["client/src"],
   "setupFilesAfterEnv": [
-    "<rootDir>/spec/client/item-details.js",
-    "<rootDir>/spec/client/questions.js",
-    "<rootDir>/spec/client/gift-now.js",
-    "<rootDir>/spec/client/shipping.js",
-    "<rootDir>/spec/client/sizing.js",
-  ]
+    "<rootDir>/spec/client/item-details-test.js",
+    "<rootDir>/spec/client/questions-test.js",
+    "<rootDir>/spec/client/gift-now-test.js",
+    "<rootDir>/spec/client/shipping-test.js",
+    "<rootDir>/spec/client/sizing-test.js",
+  ],
+  "testRegex": "(/__tests__/.*|(\\.|/)(gift-now-test))\\.js$",
 };
+
+//change something on one of these to make sure it actually heresHowItWorks
+
+// push up
+
+//add to circleCI
