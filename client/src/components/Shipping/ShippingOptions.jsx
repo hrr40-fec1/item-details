9fe160aa29ca3093bf0f62fc2b6f135c33bb3474
dj12589 <daria.jammal@gmail.com>
2019-09-04 21:43:15 -0700

    css fix, red underline, shipping options

--- client/src/components/Shipping/ShippingOptions.jsx
+++ client/src/components/Shipping/ShippingOptions.jsx
@@ -4,11 +4,15 @@ import React from 'react';
 function ShippingOptions({ details }) {
   if (details && details[0].fastShipping) {
     var newDate = moment().add(2, 'day').format('dddd, MMMM Do');
+    var extraVerbiage = 'with express shipping';
+  } else {
+    var newDate = moment().add(5, 'day').format('dddd, MMMM Do');
+    var extraVerbiage = 'with regular shipping'
   }
   return (
     <div className="shippingOptions">
       <h3>Shipping options</h3>
-      <p><span className='getItBy'>Get it by {newDate} </span>with express shipping</p>
+      <p><span className='getItBy'>Get it by {newDate} </span>{extraVerbiage}</p>
     </div>
   );
 }
