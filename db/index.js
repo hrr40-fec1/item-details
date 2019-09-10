c30b2e99072130597ce11cf46d97038192760fe1
dj12589 <daria.jammal@gmail.com>
2019-09-09 22:32:54 -0700

    db remove close

--- db/index.js
+++ db/index.js
@@ -66,7 +66,6 @@ db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', () => {
   console.log('Database and server are connected!');
-  db.close();
 });
 
 module.exports.Sizing = Sizing;
