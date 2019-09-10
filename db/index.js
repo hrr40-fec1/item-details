f5e39395e19ea70b7cd7f0affda90f9cfbd32060
Daria Jammal <35409260+dj12589@users.noreply.github.com>
2019-09-09 22:14:39 -0700

    Update index.js

--- db/index.js
+++ db/index.js
@@ -66,6 +66,7 @@ db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', () => {
   console.log('Database and server are connected!');
+  db.close();
 });
 
 module.exports.Sizing = Sizing;
